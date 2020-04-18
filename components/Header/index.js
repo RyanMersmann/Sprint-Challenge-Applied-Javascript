94c3710e4fb589988aa8da2cf6df44109f955fab
Ryan <mersmannryan@gmail.com>
2020-04-18 10:41:17 -0600

    created head component with comments for clarity

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,35 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+//create variable that selects the header-container element
+const headerContainer = document.querySelector('.header-container');
+//appends the headerContainers variable that selects the header--container element
+headerContainer.appendChild(Header());
+
+//Function that returns the div above
+function Header() {
+    //create Elements on the page
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    //adds class to elements
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //adds text content
+    date.textContent = 'SMARCH 28, 2019';
+    title.textContent = "Lambda Times";
+    temp.textContent = '98°';
+
+    //appends the child elements to the parent header div
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    // return the function
+    return header;
+
+}
